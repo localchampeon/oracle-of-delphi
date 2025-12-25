cef114e409caefd478b31b9ed13f2f2d57c3f980
Abubakar Yahya Ibrahim <65118338+localchampeon@users.noreply.github.com>
2025-12-25 10:29:42 +0100

    Create SCHEMA.sql
    create a table in the database to load the data

---
+++ scripts/SCHEMA.sql
@@ -0,0 +1,16 @@
+USE OracleOfDelphi
+
+CREATE TABLE transactions(
+	TransactionID INT IDENTITY(1,1) PRIMARY KEY,
+	InvoiceNo VARCHAR(50),
+	StockCode VARCHAR(50),
+	Description VARCHAR(300),
+	InvoiceDate DATETIME,
+	Quantity INT,
+	UnitPrice INT,
+	Revenue INT,
+	CustomerID VARCHAR(50),
+	Country VARCHAR(50),
+	SalesChannel VARCHAR(50),
+	UNIQUE (InvoiceNo, StockCode, CustomerID)
+);
