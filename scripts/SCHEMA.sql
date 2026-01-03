33ea68e15bf792ce540065c5a9911c9a6d97f512
Abubakar Yahya Ibrahim <65118338+localchampeon@users.noreply.github.com>
2026-01-03 20:13:34 +0100

    Update SCHEMA.sql
    introduced index for faster loading into the database

--- scripts/SCHEMA.sql
+++ scripts/SCHEMA.sql
@@ -1,16 +1,20 @@
-USE OracleOfDelphi
+use OracleOfDelphi
 
-CREATE TABLE transactions(
-	transactionid INT IDENTITY(1,1) PRIMARY KEY,
-	invoiceno VARCHAR(50),
-	stockcode VARCHAR(50),
-	description VARCHAR(300),
-	invoicedate DATETIME,
-	quantity INT,
-	unitprice INT,
-	revenue INT,
-	customerid VARCHAR(50),
-	country VARCHAR(50),
-	saleschannel VARCHAR(50),
-	UNIQUE (invoiceno, description)
+CREATE TABLE sales_hybrid (
+    invoiceid INT IDENTITY(1,1) PRIMARY KEY,
+    invoiceno VARCHAR(20) NULL,
+    invoicedate DATETIME NULL,
+    stockcode VARCHAR(20),
+    description VARCHAR(255),
+    quantity INT NULL,
+    unitprice DECIMAL(10,2) NULL,
+    revenue DECIMAL(18,2) NULL,
+    customerid VARCHAR(20),
+    country VARCHAR(50),
+    saleschannel VARCHAR(10) CHECK (saleschannel IN ('online', 'offline')),
+    UNIQUE (invoiceno, description),
+    
+    -- Indexes
+    INDEX idx_invoice_date (invoicedate),
+    INDEX idx_customer (customerid)
 );
